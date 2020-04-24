3d80d7861e1aedee748c3afa1154bba2bbc20cf3
theixgamer <theofritaku@gmail.com>
2020-04-24 20:27:34 -0300

    project part 1

---
+++ LeyendaTheoProject/Source/LeyendaTheoProject/LeyendaTheoProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeyendaTheoProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALeyendaTheoProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALeyendaTheoProjectGameMode();
+};
+
+
+
